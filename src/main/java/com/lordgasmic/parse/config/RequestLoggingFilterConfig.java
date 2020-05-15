dd8bdfea915c8ecafd293a99bd14efb50e020f16
Nick Wellman <wellman.nicholas@gmail.com>
2020-05-15 14:39:18 -0400

    adding logging config

---
+++ src/main/java/com/lordgasmic/parse/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,20 @@
+package com.lordgasmic.parse.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setIncludeHeaders(true);
+        filter.setAfterMessagePrefix("REQUEST DATA: ");
+
+        return filter;
+    }
+}
